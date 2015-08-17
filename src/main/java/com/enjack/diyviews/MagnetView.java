8f6ad740fb98d935d27ecbd33564485468704ef5
enjack <457003530@qq.com>
2015-08-17 17:26:42 +0800

    v1.4.0
    【添加】MagnetView

---
+++ src/main/java/com/enjack/diyviews/MagnetView.java
@@ -0,0 +1,22 @@
+package com.enjack.diyviews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by enjack on 2015/8/17.
+ */
+public class MagnetView extends SimpleView{
+
+    public MagnetView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public MagnetView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MagnetView(Context context) {
+        super(context);
+    }
+}
